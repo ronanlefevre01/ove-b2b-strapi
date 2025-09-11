c914044316bb17cd1ce93877780c0f2ff25598da
ronanlefevre01 <ronan.lefevre08@gmail.com>
2025-09-11 17:12:19 +0200

    Expose explicit /api/brands routes

--- src/api/brand/routes/brand.js
+++ src/api/brand/routes/brand.js
@@ -1,2 +1,35 @@
-import { factories } from '@strapi/strapi';
-export default factories.createCoreRouter('api::brand.brand');
+// src/api/brand/routes/brand.js
+export default {
+  routes: [
+    {
+      method: 'GET',
+      path: '/brands',
+      handler: 'brand.find',
+      config: { auth: true }, // ou false si tu veux accessible sans token
+    },
+    {
+      method: 'GET',
+      path: '/brands/:id',
+      handler: 'brand.findOne',
+      config: { auth: true },
+    },
+    {
+      method: 'POST',
+      path: '/brands',
+      handler: 'brand.create',
+      config: { auth: true },
+    },
+    {
+      method: 'PUT',
+      path: '/brands/:id',
+      handler: 'brand.update',
+      config: { auth: true },
+    },
+    {
+      method: 'DELETE',
+      path: '/brands/:id',
+      handler: 'brand.delete',
+      config: { auth: true },
+    },
+  ],
+};
